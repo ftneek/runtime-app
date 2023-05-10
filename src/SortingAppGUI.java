6b6cc3966e29f127a7ca53d0964bfe0512c80137
chiphanmsda <128106223+ChristianChiNguyen@users.noreply.github.com>
2023-05-09 20:26:35 -0700

    Add function to loop through different sizes of array
    Add function to loop through different sizes of array and input the run time in Milliseconds

--- src/SortingAppGUI.java
+++ src/SortingAppGUI.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -10,53 +10,55 @@ public class SortingAppGUI {
 
         // UI Design using swing and awt frameworks
         String[] algorithms = {"BubbleSort","SelectionSort","QuickSort","RadixSort","MergeSort","HeapSort"};
-        int[] N = {5,50,500,5000,50000};
 
-        JFrame frame = new JFrame("Runtime Analyzer");
+        JFrame frame = new JFrame("Runtime Analyzer (in milliseconds)");
         frame.setLayout(new BorderLayout());
 
         JPanel bottomPanel = new JPanel(new BorderLayout());
 
         frame.add(bottomPanel,BorderLayout.SOUTH);
 
-        DefaultTableModel model = new DefaultTableModel(new Object[] {"Algorithm (Runtime (ms))", "N = 5", "N = 50", "N = 500", "N = 5000", "N = 50000"},0) {
+        DefaultTableModel model = new DefaultTableModel(new Object[] {"Size\\Algorithms", "BubbleSort","SelectionSort","QuickSort","RadixSort","MergeSort","HeapSort"},0) {
             @Override
             public boolean isCellEditable(int row, int column) {
-                return false;
+                return column == 0;
             }
         };
 
         JTable table = new JTable(model);
-        table.setAutoCreateRowSorter(true);
+        // Resize columns
+        table.getColumnModel().getColumn(0).setPreferredWidth(100);
 
         JScrollPane scrollPane = new JScrollPane(table);
 
         frame.add(scrollPane,BorderLayout.CENTER);
-        for (String name: algorithms) {
-            model.addRow(new Object[]{name});
+        for (int i = 1; i < 6; i++) {
+            model.addRow(new Object[]{"ArraySize" + i});
         }
 
         // Create button to generate recorded time per sorting
-        Button generateButton = new Button("Sort");
-
+        JButton generateButton = new JButton("Sort");
 
         // Return values when click button "Sort"
         ActionListener listener = e -> {
-            // Initialise an array with random integers & input size
-            int size = N[4];
+            for (int i = 0; i < 5; i++) {
+                // Initialise an array with random integers & input size
+                try {
+                    int size = Integer.parseInt(table.getValueAt(i, 0).toString());
+                    int[] array = RandomArray.initialise(size);
 
-            int[] array = RandomArray.initialise(size);
+                    // Return time recorded per sorting algorithm as a HashMap
+                    HashMap<String, Long> timePerSorting = RecordTimesOfSorts.returnTimesRecorded(array);
 
-            // Return time recorded per sorting algorithm as a HashMap
-            HashMap<String, Long> timePerSorting = RecordTimesOfSorts.returnTimesRecorded(array);
-
-            for (int i = model.getRowCount() -1; i >-1 ; i--) {
-                model.removeRow(i);
-            }
-
-            // Add time recorded per sorting to the table
-            for (String name: algorithms) {
-                model.addRow(new Object[]{name, timePerSorting.get(name)});
+                    // Add time recorded per sorting to the table
+                    for (String name: algorithms) {
+                        table.setValueAt(timePerSorting.get(name), i, getColumnByName(table, name));
+                    }
+                }
+                catch(Exception exception) {
+                    JOptionPane.showMessageDialog(frame, "Please input array size as an integer to continue!",
+                            "Swing Tester", JOptionPane.ERROR_MESSAGE);
+                }
             }
         };
 
@@ -66,5 +68,13 @@ public class SortingAppGUI {
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    // Function to get column of JTable by name
+    private static int getColumnByName(JTable table, String name) {
+        for (int i = 0; i < table.getColumnCount(); ++i)
+            if (table.getColumnName(i).equals(name))
+                return i;
+        return -1;
     }
 }
